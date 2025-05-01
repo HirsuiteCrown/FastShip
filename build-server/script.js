e18ad261ed823efef97a823ec6d954562c99a33a
Aryan Dumyan <aryandumyan@Aryans-MacBook-Air.local>
2025-05-01 15:03:47 +0530

    add redis for logs pushing

--- build-server/script.js
+++ build-server/script.js
@@ -7,6 +7,13 @@ const path = require('path')
 const fs = require('fs')
 const {S3Client, PutObjectCommand} = require('@aws-sdk/client-s3')
 const mime = require('mime-types')
+const Redis = require('ioredis')
+
+const publisher = new Redis();
+
+function publishLog(log){
+    publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify({log}));
+}
 
 const s3Client = new S3Client({
     region: '',
@@ -20,6 +27,7 @@ const PROJECT_ID = process.env.PROJECT_ID;
 
 async function init(){
     console.log("Executing script.js")
+    publishLog('Build Started...')
     const outDirPath = path.join(__dirname, 'output');
 
     const p = exec(`cd ${outDirPath} && npm install && npm run build`)
@@ -27,18 +35,23 @@ async function init(){
     //package install krte time jo bhi logs aayenge unko captur krte hai:-
     p.stdout.on('data', function(data){
         console.log(data.toString());
+        publishLog(data.toString());
     })
     p.stderr.on('data', function(data){
         console.log('Error', data.toString());
+        publishLog(`error: ${data.toString()}`);
     })    
     p.on('close',async function(){
         console.log('Build Complete')
+        publishLog('Build Complete');
         //after build we have to put dist file into S3
         const distFolderPath = path.join(__dirname,'output','dist')
         const distFolderContents = fs.readdirSync(distFolderPath,{recursive: true})
 
         //when we upload on S3 we have to give file path not folder.
+        publishLog('Starting to upload');
         for(const file of distFolderContents){
+            publishLog.publish(`uploading file ${file}`);
             const filePath = path.join(distFolderPath,file);
             if(fs.lstatSync(filePath).isDirectory()) continue;
             //if folder then continue else upload on s3
@@ -51,7 +64,10 @@ async function init(){
             })
 
             await s3Client.send(command);
+            publishLog.publish(`uploaded ${file}`);
+            console.log('uploaded', filePath);
         }
+        publishLog.publish('Done');
         console.log('Done...')
     })
 }
